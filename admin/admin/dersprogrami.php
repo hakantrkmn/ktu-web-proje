abae65365c48d891234180cf8f2d316490a2873e
hakantrkmn <hakan_61_601@hotmail.com>
2019-05-05 13:36:59 +0300

    ;)

--- admin/admin/dersprogrami.php
+++ admin/admin/dersprogrami.php
@@ -19,76 +19,6 @@ include 'header.php';
 
  ?>
 
-  <body class="nav-md">
-    <div class="container body">
-      <div class="main_container">
-        <div class="col-md-3 left_col">
-          <div class="left_col scroll-view">
-
-
-            <div class="clearfix"></div>
-
-            <!-- menu profile quick info -->
-            <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="https://s3.amazonaws.com/prod.skimble/photos/29359/hstzsdw4avx_full.gif" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Welcome  <a href="../../cikis.php"> <i class="fas fa-sign-out-alt"></i></a> </span>
-                <h2> <?php echo $kullanicicek['k_ad'] ?> </h2>
-              </div>
-            </div>
-            <!-- /menu profile quick info -->
-
-            <br />
-
-            <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-                    <div class="menu_section">
-                      <h3>General</h3>
-                      <ul class="nav side-menu">
-                        <li><a href="duyuru.php"><i class="fa fa-home"></i> duyuru <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                        <li><a href="video.php"><i class="fa fa-home"></i> video <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                        <li><a href="etkinlik.php"><i class="fa fa-home"></i> etkinlik <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                        <li><a href="dersprogrami.php"><i class="fa fa-home"></i> ders <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                        <li><a href="resim.php"><i class="fa fa-home"></i> resim <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                      </ul>
-                    </div>
-      
-                  </div>
-            <!-- /sidebar menu -->
-          </div>
-        </div>
-
-        <!-- top navigation -->
-        <div class="top_nav">
-          <div class="nav_menu">
-            <nav>
-              <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-              </div>
-
-              <ul class="nav navbar-nav navbar-right">
-                <li class="">
-                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="https://s3.amazonaws.com/prod.skimble/photos/29359/hstzsdw4avx_full.gif" alt=""><?php echo $kullanicicek['k_ad'] ?>
-                    <span class=" fa fa-angle-down"></span>
-                  </a>
-                  <ul class="dropdown-menu dropdown-usermenu pull-right"> 
-                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <!-- /top navigation -->
-
         <!-- page content -->
         <div class="right_col" role="main">
           <div class="">
@@ -101,29 +31,15 @@ include 'header.php';
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>Ders Programı</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
+
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
                     <table id="datatable" class="table table-striped table-bordered">
                       <thead>
                         <tr>
-                          <th>program linki</th>
-                          <th>sil</th>
+                          <th>Program linki</th>
+                          <th>Sil</th>
                         </tr>
                       </thead>
 
@@ -132,17 +48,20 @@ include 'header.php';
                         <?php foreach($dersprogramicek as $dersprogrami) :?>
                         <tr>
                           <td><?php echo $dersprogrami->img ?></td>
-                          <td><a href="../../islem.php?derssil=ok&kullanici_id=<?php echo $dersprogrami->k_id ?>&ders_id=<?php echo $dersprogrami->ders_id ?>"><button class="btn btn-secondary">sil</button></a></td>
+                          <td><a class="btn btn-danger" href="../../islem.php?derssil=ok&kullanici_id=<?php echo $dersprogrami->k_id ?>&ders_id=<?php echo $dersprogrami->ders_id ?>"><button class="btn btn-secondary">sil</button></a></td>
 
                         </tr>
                         <?php endforeach ?>
                       </tbody>
                     </table>
                   </div>
+                    <div style="text-align:right;">
+                     <a  class="btn btn-success" href="ekleders.php">yeni ekle</a>
+                   </div>
                 </div>
               </div>
             </div>
-            <a href="ekleders.php">yeni ekle</a>
+
           </div>
         </div>
         <!-- /page content -->
--- admin/admin/resim.php
+++ admin/admin/resim.php
@@ -27,98 +27,10 @@ include 'header.php';?>
 
 
 
-
-  <body class="nav-md">
-    <div class="container body">
-      <div class="main_container">
-        <div class="col-md-3 left_col">
-          <div class="left_col scroll-view">
-            <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
-            </div>
-
-            <div class="clearfix"></div>
-
-            <!-- menu profile quick info -->
-            <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Welcome  <a href="../../cikis.php"> <i class="fas fa-sign-out-alt"></i></a> </span>
-                <h2> <?php echo $kullanicicek['k_ad'] ?></h2>
-
-              </div>
-            </div>
-            <!-- /menu profile quick info -->
-
-            <br />
-
-            <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-                    <div class="menu_section">
-                      <h3>General</h3>
-                      <ul class="nav side-menu">
-                        <li><a href="duyuru.php"><i class="fa fa-home"></i> duyuru <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                        <li><a href="video.php"><i class="fa fa-home"></i> video <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                        <li><a href="etkinlik.php"><i class="fa fa-home"></i> etkinlik <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                        <li><a href="dersprogrami.php"><i class="fa fa-home"></i> ders <span class="fa fa-chevron-down"></span></a>
-                        </li>
-                        <li><a href="resim.php"><i class="fa fa-home"></i> resim <span class="fa fa-chevron-down"></span></a>
-
-                      </ul>
-                    </div>
-      
-                  </div>
-            <!-- /sidebar menu -->
-          </div>
-        </div>
-
-        <!-- top navigation -->
-        <div class="top_nav">
-          <div class="nav_menu">
-            <nav>
-              <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-              </div>
-
-              <ul class="nav navbar-nav navbar-right">
-                <li class="">
-                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">John Doe
-                    <span class=" fa fa-angle-down"></span>
-                  </a>
-                  <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Profile</a></li>
-                    <li>
-                      <a href="javascript:;">
-                        <span class="badge bg-red pull-right">50%</span>
-                        <span>Settings</span>
-                      </a>
-                    </li>
-                    <li><a href="javascript:;">Help</a></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <!-- /top navigation -->
-
         <!-- page content -->
         <div class="right_col" role="main">
           <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Users <small>Some examples to get you started</small></h3>
-              </div>
-
-
-            </div>
+           
 
             <div class="clearfix"></div>
 
@@ -126,19 +38,11 @@ include 'header.php';?>
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Default Example <small>Users</small></h2>
+                    <h2>Resimler</h2>
                     <ul class="nav navbar-right panel_toolbox">
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                       </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
+                      
                       <li><a class="close-link"><i class="fa fa-close"></i></a>
                       </li>
                     </ul>
@@ -148,9 +52,9 @@ include 'header.php';?>
                     <table id="datatable" class="table table-striped table-bordered">
                       <thead>
                         <tr>
-                          <th>resim</th>
-                          <th>yazısı</th>
-                          <th>sil</th>
+                          <th>Resim</th>
+                          <th>Yazısı</th>
+                          <th>Sil</th>
                         </tr>
                       </thead>
 
@@ -170,11 +74,13 @@ include 'header.php';?>
                       </tbody>
                     </table>
                   </div>
+                  <div style="text-align:right">
+                    <a class="btn btn-success" href="ekleresim.php">yeni ekle</a>
+                  </div>
                 </div>
               </div>
             </div>
-            <a href="ekleresim.php">yeni ekle</a>
-
+            
           </div>
         </div>
         <!-- /page content -->
